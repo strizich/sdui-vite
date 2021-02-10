4613d2ab2be15bf1b32a2c5bc884c47214d1839d
Jason Strizich <strizich@me.com>
2021-02-10 16:36:39 -0600

    docs styling

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,23 +1,41 @@
 <template>
-  <SdLayout>
+  <sd-layout class="sd--docs" floating :sidebar="state.sidebar">
+    <template v-slot:header>
+      <sd-header @toggle="toggleSidebar"/>
+    </template>
+    <template v-slot:sidebar>
+      Im here
+    </template>
     <template v-slot:content>
       <sd-container>
         <Content/>
       </sd-container>
     </template>
-  </SdLayout>
+  </sd-layout>
 </template>
 
 <script>
+  import SdHeader from '../../../src/lib/components/SdLayout/SdHeader.vue'
   import SdLayout from '../../../src/lib/components/SdLayout/SdLayout.vue'
   import SdContainer from '../../../src/lib/components/SdGrid/SdContainer.vue'
+  import { reactive } from 'vue'
   export default {
     name: 'Layout',
-    components: { SdLayout, SdContainer }
+    components: { SdLayout, SdHeader, SdContainer },
+    setup() {
+      const state = reactive({
+        sidebar: false
+      })
+      const toggleSidebar = () => {
+        state.sidebar = !state.sidebar
+      }
+      return { state, toggleSidebar }
+    }
   }
 </script>
 
 <style lang="scss">
   @use '../src/lib/scss/engine.scss';
+  @use 'scss/docs';
   @import url('../../../node_modules/prismjs/themes/prism-dark.css');
 </style>
