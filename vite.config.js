8953a7695a28dd0b6bdc9db899554d29bbe74a73
Jason Strizich <strizich@me.com>
2021-02-10 13:47:17 -0600

    - added initial lib build settings - commented out until issues bugs in vue are resolved

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,44 @@ import vue from '@vitejs/plugin-vue'
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
+
 export default {
   plugins: [vue()]
 }
+
+// LIBRARY BUILD WAITING ON vue-3.0.6 to resolve a rollup bug.
+// https://github.com/vitejs/vite/issues/1523
+// const path = require('path')
+
+// module.exports = {
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, 'src/lib/index.js'),
+//       name: 'sdui'
+//     },
+//     rollupOptions: {
+//       // make sure to externalize deps that shouldn't be bundled
+//       // into your library
+//       external: ['vue'],
+//       output: {
+//         // Provide global variables to use in the UMD build
+//         // for externalized deps
+//         globals: {
+//           vue: 'Vue'
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+// GOES INTO PACKAGE.JSON
+// "files": ["dist"],
+// "main": "./dist/sdui.umd.js",
+// "module": "./dist/sdui.es.js",
+// "exports": {
+//   ".": {
+//     "import": "./dist/sdui.es.js",
+//     "require": "./dist/sdui.umd.js"
+//   }
+// },
