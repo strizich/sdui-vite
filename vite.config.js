29faf15d242357b5fd1232b0a1c3332266066e67
Jason Strizich <strizich@me.com>
2021-02-26 13:45:26 -0600

    library packaging configuration

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,44 @@
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 /**
- * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 
-export default {
-  alias: {
-    '@': require('path').resolve(__dirname, 'src'),
-    '@lib/': require('path').resolve(__dirname, 'src/lib')
+const path = require('path')
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@lib/': path.resolve(__dirname, 'src/lib')
+    }
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        { src: 'src/lib/scss/**/*', dest: 'dist/scss' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  build: {
+    lib: {
+      name: 'sdui',
+      entry: path.resolve(__dirname, 'src/lib/index.js')
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 }
-
-// LIBRARY BUILD WAITING ON vue-3.0.6 to resolve a rollup bug.
-// https://github.com/vitejs/vite/issues/1523
-// const path = require('path')
-
-// module.exports = {
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, 'src/lib/index.js'),
-//       name: 'sdui'
-//     },
-//     rollupOptions: {
-//       // make sure to externalize deps that shouldn't be bundled
-//       // into your library
-//       external: ['vue'],
-//       output: {
-//         // Provide global variables to use in the UMD build
-//         // for externalized deps
-//         globals: {
-//           vue: 'Vue'
-//         }
-//       }
-//     }
-//   }
-// }
-
-
-// GOES INTO PACKAGE.JSON
-// "files": ["dist"],
-// "main": "./dist/sdui.umd.js",
-// "module": "./dist/sdui.es.js",
-// "exports": {
-//   ".": {
-//     "import": "./dist/sdui.es.js",
-//     "require": "./dist/sdui.umd.js"
-//   }
-// },
