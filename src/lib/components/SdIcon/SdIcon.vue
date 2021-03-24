47ac21cfbdb6a4c748610de6976d7472056c6b14
Jason Strizich <strizich@me.com>
2021-03-24 17:38:19 -0500

    use material icons pkg instead cdn version
    add node_modules to aliases

--- src/lib/components/SdIcon/SdIcon.vue
+++ src/lib/components/SdIcon/SdIcon.vue
@@ -1,6 +1,7 @@
 <template>
-  <i :class="['sd--icon', iconSize]" :style="{'color': color}">
+  <i :class="['sd--icon', iconClasses]" :style="{'color': color}">
     {{name}}
+    <slot/>
   </i>
 </template>
 
@@ -13,6 +14,9 @@ export default defineComponent({
       type: String,
       default: 'face'
     },
+    family: {
+      type: String,
+    },
     size: {
       type: String,
       default: 'md'
@@ -22,15 +26,23 @@ export default defineComponent({
     }
   },
   setup (props) {
-    const iconSize = computed(() => {
-      return `is--${props.size}`
+    const iconClasses = computed(() =>{
+      const iconSize = `is--${props.size}`
+      const iconFamily = !props.family ? 'material-icons' : `material-icons-${props.family}`
+    return {
+      [iconFamily]: true,
+      [iconSize]: !!props.size
+    }
     })
-    return { iconSize }
+    return { iconClasses }
   }
 })
 </script>
 
 <style lang="scss">
+$material-icons-font-path: '~/material-icons/iconfont/';
+@import '~/material-icons/iconfont/material-icons.scss';
+
 $icon-sizes: (
   xs: 16px,
   sm: 18px,
@@ -39,29 +51,8 @@ $icon-sizes: (
   xl: 32px
 );
 
-@font-face {
-  font-family: 'Material Icons';
-  font-style: normal;
-  font-weight: 400;
-  src: url(https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2) format('woff2');
-}
-
 .sd--icon {
-  font-family: 'Material Icons';
-  font-weight: normal;
-  font-style: normal;
-  font-size: map-get($icon-sizes, md);
-  line-height: 1;
-  letter-spacing: normal;
-  text-transform: none;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  white-space: nowrap;
-  word-wrap: normal;
-  direction: ltr;
-  font-feature-settings: 'liga';
-  font-smooth: antialiased;
+  color: var(--text);
   &.is{
     @each $size, $value in $icon-sizes{
       &--#{$size}{
@@ -74,5 +65,4 @@ $icon-sizes: (
     }
   }
 }
-
 </style>
