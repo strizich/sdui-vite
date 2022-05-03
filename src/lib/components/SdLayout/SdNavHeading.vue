63e784c92ef6599e6b855ca1ee4e7ebfc596ee0f
Jason Strizich <strizich@me.com>
2022-05-03 12:12:53 -0500

    fix bad imports

--- src/lib/components/SdLayout/SdNavHeading.vue
+++ src/lib/components/SdLayout/SdNavHeading.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "@vue/runtime-core"
+import { defineComponent } from "vue"
 
 export default defineComponent({
   name: 'SdNavHeading'
